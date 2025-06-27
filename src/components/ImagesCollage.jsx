de0f7fd4386ea05c2cb17ad48699d4439271047e
Rahul Rasve <rahul.rasve@scriptanalytica.com>
2025-06-27 13:38:55 +0530

    story and team section done

--- src/components/ImagesCollage.jsx
+++ src/components/ImagesCollage.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const ImagesCollage = ({images = []}) => {
+const ImagesCollage = ({ images = [] }) => {
   return (
-    <div className={`grid grid-rows-2 grid-flow-row p-2`}>
-      {
-        images.map((element, index) => (
-          <div key={index} className={`h-[27rem] w-[22rem] p-[2rem]`}>
-            <img key={element.label} src={element.imagePath} alt={element.label} />
-          </div>
-        ))
-      }
+    <div className={`w-full grid grid-rows-2 grid-flow-col `}>
+      {images.map((element, index) => (
+        <div
+          key={index}
+          className={`h-[27rem] w-[22rem] p-[2rem] flex ${
+            index === 2 || index === 3 ? "items-start mt-4" : "items-center"
+          }  `}
+        >
+          <img
+            key={element.label}
+            src={element.imagePath}
+            alt={element.label}
+            className={`h-[25rem] w-[20rem] object-cover rounded-xl transition-transform duration-300 ease-in-out hover:scale-105 `}
+          />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ImagesCollage
+export default ImagesCollage;
--- src/screens/AboutUs/components/StoryAndTeam.jsx
+++ src/screens/AboutUs/components/StoryAndTeam.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { story, team } from "../../../utils/text-constants";
+import ImagesCollage from "../../../components/ImagesCollage";
 
 const StoryAndTeam = () => {
   return (
@@ -35,7 +36,9 @@ const StoryAndTeam = () => {
         </div>
 
         {/* Team Images */}
-        <div className={`h-[20vh] border border-black w-[75%]`}></div>
+        <div className={`h-fit w-fit`}>
+          <ImagesCollage images={imagesList} />
+        </div>
       </div>
     </div>
   );
@@ -44,10 +47,10 @@ const StoryAndTeam = () => {
 export default StoryAndTeam;
 
 const imagesList = [
-  {label: "person-one", imagePath: "/images/team/person-one.jpeg"},
-  {label: "person-one", imagePath: "/images/team/person-one"},
-  {label: "person-one", imagePath: "/images/team/person-one"},
-  {label: "person-one", imagePath: "/images/team/person-one"},
-  {label: "person-one", imagePath: "/images/team/person-one"},
-  {label: "person-one", imagePath: "/images/team/person-one"},
+  { label: "person-one", imagePath: "/images/team/person-one.jpg" },
+  { label: "person-two", imagePath: "/images/team/person-two.png" },
+  { label: "person-three", imagePath: "/images/team/person-three.png" },
+  { label: "person-four", imagePath: "/images/team/person-four.png" },
+  { label: "person-five", imagePath: "/images/team/person-five.png" },
+  { label: "person-six", imagePath: "/images/team/person-six.png" },
 ];
